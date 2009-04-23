4dcaf494b264c8d16295c1b373f359a12ca156af
danilo.tsr <danilo.tsr@7aa32740-1c9c-11de-9909-f37ff7dbbc12>
2009-04-23 01:19:33 +0000

    Initial import.

---
+++ MyMoney-M2/src/model/MyMoneyFacade.java
@@ -0,0 +1,165 @@
+package model;
+
+public class MyMoneyFacade {
+
+	// Utils
+	
+	public void quit() {
+	}
+	
+	public void cleanAll() {
+	}
+	
+	public String getFileContent(String filePath) {
+		return null;
+	}
+
+	// US-01
+
+	public void register(String login, String password, String name,
+			String gender, String eMail) {
+	}
+	
+	public String getUserName(String login) {
+		return null;
+	}
+	
+	public String getUserGender(String login) {
+		return null;
+	}
+	
+	public String getUserEmail(String login) {
+		return null;
+	}
+	
+	public void removeUser(String login) {
+	}
+
+	// US-02
+
+	public void doLogin(String login, String password) {
+	}
+	
+	public boolean isLogged(String login) {
+		return false;
+	}
+
+	// US-03
+
+	public long createAccount(String login, String label, String agency,
+			String account) {
+		return 0;
+	}
+
+	// US-04
+
+	public long addOperationIntoDefaultAccount(String login, String type,
+			String way, double amount) {
+		return 0;
+	}
+	
+	public long addOperation(String login, long accId, String type, String way, double amount) {
+		return 0;
+	}
+
+	public String getOperationType(long opId) {
+		return null;
+	}
+
+	public String getOperationWay(long opId) {
+		return null;
+	}
+
+	public double getOperationAmount(long opId) {
+		return 0.0;
+	}
+	
+	public long getNumberOfOperations(String login) {
+		return 0;
+	}
+	
+	// US-05
+	
+	public double getDefAccOverallAmount() {
+		return 0.0;
+	}
+	
+	public double getAccOverallAmount() {
+		return 0.0;
+	}
+	
+	// US-06
+	
+	public void removeOperation(String login, long idOperation) {
+	}
+	
+	// US-07
+	
+	public void removeAccount(String login, long id) {
+	}
+	
+	// US-08
+	
+	public long addCommitment(String login, String label, String date, double amount, String type, String frequency) {
+		return 0;
+	}
+	
+	public String getCommitmentLabel(String login, long id) {
+		return null;
+	}
+	
+	public String getCommitmentDate(String login, long id) {
+		return null;
+	}
+	
+	public double getCommitmentAmount(String login, long id) {
+		return 0.0;
+	}
+	
+	public String getCommitmentType(String login, long id) {
+		return null;
+	}
+	
+	public String getCommitmentFrequency(String login, long id) {
+		return null;
+	}
+	
+	public long numberOfCommitments(String  login) {
+		return 0;
+	}
+	
+	public void removeCommitment(String login, long id) {
+	}
+	
+	// US - 09
+	
+	public void submitBankOperations(String login, String fileContent) {
+	}
+	
+	// US-10
+	
+	public void generateReport(String login, String begin, String end, String operationType, long idAccount, boolean commitments) {
+	}
+	
+	public int getNumberOfOperationsReport() {
+		return 0;
+	}
+
+	public int getNumberOfCreditOperationsReport() {
+		return 0;
+	}
+
+	public int getNumberOfDebtOperationsReport() {
+		return 0;
+	}
+
+	public int getNumberOfCommitmentsReport() {
+		return 0;
+	}
+	
+	// US-11
+	
+	public void doLogoff(String login, String password) {
+	}
+
+}
